26c07eec608bb31e07a1ade53f5c562767ecb127
Michael Frohlich <mike.frohl@gmail.com>
2026-04-14 16:47:51 +0200

    Add design tokens and global CSS with RTL-friendly utilities

---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './styles/tokens.css';
+import './styles/global.css';
+import './lib/i18n.js';
+import App from './App.jsx';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
